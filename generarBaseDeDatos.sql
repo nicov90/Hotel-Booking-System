3123bc49c5ef17460f8dcb59ae22ab51ec9278f4
Nicolás Valdez <nico_v99@hotmail.com>
2023-02-14 20:11:38 -0300

    -

---
+++ generarBaseDeDatos.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE hotel_alura1;
+USE hotel_alura1;
+
+CREATE TABLE reservas(
+	ID INT NOT NULL,
+    FechaEntrada VARCHAR(50) NOT NULL,
+    FechaSalida VARCHAR(50) NOT NULL,
+    Valor DOUBLE NOT NULL,
+    MetodoDePago VARCHAR(50) NOT NULL,
+    PRIMARY KEY (ID)
+);
+CREATE TABLE huespedes(
+	ID INT AUTO_INCREMENT,
+    Nombre VARCHAR(50) NOT NULL,
+    Apellido VARCHAR(50) NOT NULL,
+    FechaNacimiento VARCHAR(45) NOT NULL,
+    Nacionalidad VARCHAR(50) NOT NULL,
+    Telefono VARCHAR(50) NOT NULL,
+    IdReserva INT NOT NULL,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (IdReserva) REFERENCES reservas(ID)
+);
